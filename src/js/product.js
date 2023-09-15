432d38f5768c5fb9f5159f7c28e53e1a16037675
Ronald <ronaldcoello85@gmail.com>
2023-09-14 23:48:41 -0600

    fixed-cart

--- src/js/product.js
+++ src/js/product.js
@@ -1,10 +1,16 @@
-import { setLocalStorage } from './utils.mjs';
+import { getLocalStorage, setLocalStorage } from './utils.mjs';
 import ProductData from './ProductData.mjs';
 
 const dataSource = new ProductData('tents');
 
 function addProductToCart(product) {
-  setLocalStorage('so-cart', product);
+  let oldCartContent = getLocalStorage('so-cart')
+  if(oldCartContent) {
+    setLocalStorage('so-cart', [...oldCartContent, product]);
+  } else {
+    setLocalStorage('so-cart', [product])
+  }
+  
 }
 // add to cart button event handler
 async function addToCartHandler(e) {
