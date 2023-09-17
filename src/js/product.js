2e8fd7845ecb9a5269db683cbe1b05dcff369912
Alexander Nelson <alexanortega@gmail.com>
2023-09-17 01:26:29 -0600

    Update product.js

--- src/js/product.js
+++ src/js/product.js
@@ -11,13 +11,13 @@ function addProductToCart(product) {
     setLocalStorage("so-cart", [product]);
   }
 }
-// add to cart button event handler
+// add to cart button event handler 
 async function addToCartHandler(e) {
   const product = await dataSource.findProductById(e.target.dataset.id);
   addProductToCart(product);
 }
 
-// add listener to Add to Cart button
+// add listener to Add to Cart button 
 document
   .getElementById("addToCart")
   .addEventListener("click", addToCartHandler);
