ecefadfec4565c0258ebe3e0312ef125cc13c66d
Ronald <ronaldcoello85@gmail.com>
2023-09-24 00:15:41 -0600

    changes

--- src/js/product.js
+++ src/js/product.js
@@ -11,21 +11,20 @@ const productId = getParam("product");
 const product = new ProductDetails(productId, dataSource);
 product.init();
 
-// function addProductToCart(product) {
-//   let oldCartContent = getLocalStorage("so-cart");
-//   if (oldCartContent) {
-//     setLocalStorage("so-cart", [...oldCartContent, product]);
-//   } else {
-//     setLocalStorage("so-cart", [product]);
-//   }
-// }
-// // add to cart button event handler
-// async function addToCartHandler(e) {
-//   const product = await dataSource.findProductById(e.target.dataset.id);
-//   addProductToCart(product);
-// }
+function addProductToCart(product) {
+  let oldCartContent = getLocalStorage("so-cart");
+  if (oldCartContent) {
+    setLocalStorage("so-cart", [...oldCartContent, product]);
+  } else {
+    setLocalStorage("so-cart", [product]);
+  }
+}
+// add to cart button event handler
+async function addToCartHandler(e) {
+  const product = await dataSource.findProductById(e.target.dataset.id);
+  addProductToCart(product);
+}
 
 // add listener to Add to Cart button
-// document
-//   .getElementById("addToCart")
-//   .addEventListener("click", addToCartHandler);
+console.log("here")
+document.getElementById("addToCart").addEventListener("click", addToCartHandler);
