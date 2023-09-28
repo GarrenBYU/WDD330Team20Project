253c4e1db3196b4358d84a795129dd7f06fad374
Oleg Lasukov <lasukov2016@mail.ru>
2023-09-28 21:24:49 +0500

    feat: team weak 3 activity done

---
+++ src/js/ShoppingCart.mjs
@@ -0,0 +1,51 @@
+import { getLocalStorage } from "./utils.mjs";
+import { loadHeaderFooter } from "./utils.mjs";
+
+function cartItemTemplate(item) {
+  const newItem = `<li class='cart-card divider'>
+  <a href='#' class='cart-card__image'>
+    <img
+      src='${item.Image}'
+      alt='${item.Name}'
+    />
+  </a>
+  <a href='#'>
+    <h2 class='card__name'>${item.Name}</h2>
+  </a>
+  <p class='cart-card__color'>${item.Colors[0].ColorName}</p>
+  <p class='cart-card__quantity'>qty: 1</p>
+  <p class='cart-card__price'>$${item.FinalPrice}</p>
+</li>`;
+
+  return newItem;
+}
+
+
+export default class ShoppingCard {
+  constructor(key, target) {
+  this.key = key;
+  this.target =  target;
+}
+
+renderBasket(){
+  const items = getLocalStorage(this.key);
+  const htmlElements = items.map((item)=>cartItemTemplate(item));
+  document.querySelector(this.target).innerHTML = htmlElements.join("");
+}
+
+
+
+renderTotal(){
+  const products = getLocalStorage('so-cart')
+  if(!products) return;
+  let sum = 0;
+  let total_holder = document.querySelector(".hide");
+  
+  for(const obj of products)
+  {
+    obj.FinalPrice ? sum += obj.FinalPrice : sum+=0;
+  }
+  total_holder.style.display = "block";
+  total_holder.textContent = `Total: $${sum.toFixed(2)}`;
+}
+}
